8b850e4336a86ae138e575e07bffb51f5eb44f8a
Mark Svendstrup Knudsen <mark4610@edu.easj.dk>
2023-11-09 14:03:45 +0100

    app.js changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost/wpapi/wp-json/wp/v2/posts?_embed') // change this path!
+      .then((res) => setPosts(res.data))
+  }, [])
+
+  const postsJsx = posts.map((post) => (
+    <li key={post.id} dangerouslySetInnerHTML={{ __html: post.content.rendered }}></li>
+  ))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ul>{postsJsx}</ul>
+  )
 }
 
 export default App;
